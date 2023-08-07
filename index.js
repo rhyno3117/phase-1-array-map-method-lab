921bcadd8af833857760c4a8a477abefd5b06f04
Ryan Meyers <rhynogolfer42@gmail.com>
2023-08-07 14:38:14 -0600

    Adds myfile.md

--- index.js
+++ index.js
@@ -12,5 +12,20 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((line)=>{
+    const tokens = line.split(" ");
+    const capitalizedTokens =tokens.map(
+      (token)=>token.charAt(0).toUpperCase()+token.slice(1)
+    );
+    const response= capitalizedTokens.join(" ");
+    return response;
+  });
+};
+
+//const titleCased = () => { ... }: This defines a constant arrow function named titleCased with no parameters.
+
+//const response = capitalizedTokens.join(" ");: This joins the capitalizedTokens array back into a single string, using a space as the separator between words.
+
+//Finally, the mapped and formatted string (response) is returned for each line, resulting in an array of title-cased strings.
+
+//Please note that in order for this
